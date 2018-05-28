a284576884333084a4f3a283e93035fb2c803bbc
Matt Bonnyman <mbonnyman@gemini.edu>
2018-05-28 17:35:22 -0400

    Improved, but still quite slow.

--- sb.py
+++ sb.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math as m
 import astropy.units as u
 
 def ztwilight(alt):
@@ -8,8 +9,8 @@ def ztwilight(alt):
 
 def xair(z):
     degrad  =   57.2957795130823/u.rad
-    sin2 = np.power(np.sin(z/degrad),2)
-    val = 1.0/np.sqrt(1.0 - 0.96*sin2)
+    sin2 = np.sin(z/degrad)**2
+    val = 1.0/(1.0-0.96*sin2)**0.5
     return val
 
 def sb(mpa,mdist,mZD,ZD,sZD,cc):
@@ -38,30 +39,45 @@ def sb(mpa,mdist,mZD,ZD,sZD,cc):
 
     sun_alt = 90.0 - sZD #sun altitude
 
+    #print('sun_alt',sun_alt)
+
     Vzen = np.ones(len(ZD)) 
     Vzen = Vzen*21.587 #Dark sky zenith V surface brightness
-    ii = np.where(sun_alt > -18.5)
+    ii = np.where(sun_alt > -18.5)[0][:]
     Vzen[ii] = Vzen[ii] - ztwilight(sun_alt[ii]) #correction for sky brightness
-
-    Bzen = 0.263 * np.power(a,Q-Vzen) #zentih sky brightness
-    Bsky = Bzen * xair(ZD) * np.power(10.0,-0.4 * k * (xair(ZD)-1.0)) #sky constribution
+    
+    
+    Bzen = 0.263 * a**(Q-Vzen) #zentih sky brightness
+    Bsky = Bzen * xair(ZD) * 10.0**(-0.4*k*(xair(ZD)-1.0)) #sky constribution
 
     n = len(Bsky)
     istar = 0.0
     fp = 0.0
     Bmoon = np.zeros(n)
     for j in range(0,n):
+        #print('mZD',mZD[j])
+        #print('ZD',ZD[j])
+        #print('mpa',mpa)
         if mZD[j]<90.8:
-            istar = np.power(10.0, -0.4*(3.84 + 0.026 * np.absolute(mpa) + (4.0-9.0)*np.power(mpa,4.0)))
-        if mdist[j]>10.0:
-            fp = (1.06 + np.power(np.cos(mdist[j]/degrad),2)) * np.power(10.0,5.36) + np.power(10.0,6.15 - mdist[j]/40.0)
-        else:
-            fp = 6.2 / np.power(mdist[j],2) #original said fp=6.2d7/mdist[j]^2
-
-        Bmoon[j] = fp * istar * np.power(10,-0.4 * k * xair(mZD[j])) * (1.0 - np.power(10,-0.4 * k * xair(ZD[j])))
+            istar = 10.0**(-0.4*(3.84 + 0.026 * abs(mpa) + (4.0e-9)*(mpa)**4.0))
+            #print('istar',istar)
+            #print('mdist',mdist[j])
+            if mdist[j]>10.0:
+                fp = (1.06 + np.cos(mdist[j]/degrad)**2) * 10.0**5.36 + 10.0**(6.15 - mdist[j]/40.0)
+            else:
+                fp = 6.2e7 /(mdist[j]**2) #original said fp=6.2d7/mdist[j]^2
+            #print('fp',fp)
+            Bmoon[j] = fp * istar * 10**(-0.4 * k * xair(mZD[j])) * (1.0 - 10**(-0.4 * k * xair(ZD[j])))
+            #print('Bmoon',Bmoon[j])
 
     if cc!=0.5: #Very simple increase in SB if there are clouds
         Bmoon = 2.0 * Bmoon 
     
     sb = Q - np.log10( (Bmoon + Bsky)/0.263) / np.log10(a) #sky brightness in Vmag/arcsec^2
+    # print('istar',istar)
+    # print('fp',fp)
+    # print('Bmoon',Bmoon)
+    # print('Bsky',Bsky)
+    # print('sb',sb)
+
     return sb
