b461a95fd1c22befaca9ef61d657436c3d61dc66
Matt Bonnyman <mbonnyman@gemini.edu>
2018-05-28 17:31:24 -0400

    Started writing observation & group classes.

--- convcond.py
+++ convcond.py
@@ -17,14 +17,12 @@ def convert_array(iq,cc,bg,wv):
             iq[i]=1. #set all 'Any' or 'null' IQs to 1.
     iq = iq.astype('<U2') #trim array elements to 2 characters
     iq = np.asarray(iq,dtype='f8') #convert array to numpy type w/ double precision floats
-    ii = np.where(iq!=1.)[0][:] #get indices of percentage IQs
+    ii = np.where(iq!=1.)[0][:] #get indeces of percentage IQs
     iq[ii] = iq[ii]/100. #divide all IQ percentages by 100
 
-    # IQ=70 needs more relative weight, otherwise difference
-    # between IQ=70 and IQ=85 too small
-    indices = np.where(iq==0.7)[0][:]
-    if len(indices)!=0: #change 70% image qualities to 50%
-        iq[indices] = 0.5
+    indeces = np.where(iq==0.7)[0][:]
+    if len(indeces)!=0: #change 70% image qualities to 50%
+        iq[indeces] = 0.5
 
 
     #========= convert cloud condition values ==============
@@ -33,7 +31,7 @@ def convert_array(iq,cc,bg,wv):
             cc[i]=1. #set all 'Any' or 'null' CCs to 1.
     cc = cc.astype('<U2') #trim array elements to 2 characters
     cc = np.asarray(cc,dtype='f8') #convert array to numpy type w/ double precision floats
-    ii = np.where(cc!=1.)[0][:] #get indices of percentage CCs
+    ii = np.where(cc!=1.)[0][:] #get indeces of percentage CCs
     cc[ii] = cc[ii]/100. #divide all CC percentages by 100
 
 
@@ -43,7 +41,7 @@ def convert_array(iq,cc,bg,wv):
             bg[i]=1. #set all 'Any' or 'null' BGs to 1.
     bg = bg.astype('<U2') #trim array elements to 2 characters
     bg = np.asarray(bg,dtype='f8') #convert array to numpy type w/ double precision floats
-    ii = np.where(bg!=1.)[0][:] #get indices of percentage BGs
+    ii = np.where(bg!=1.)[0][:] #get indeces of percentage BGs
     bg[ii] = bg[ii]/100. #divide all BG percentages by 100
 
     #============= convert water vapour values =============
@@ -52,46 +50,13 @@ def convert_array(iq,cc,bg,wv):
             wv[i]=1. #set all 'Any' or 'null' WVs to 1.
     wv = wv.astype('<U2') #trim array elements to 2 characters
     wv = np.asarray(wv,dtype='f8') #convert array to numpy type w/ double precision floats
-    ii = np.where(wv!=1.)[0][:] #get indices of percentage WVs
+    ii = np.where(wv!=1.)[0][:] #get indeces of percentage WVs
     wv[ii] = wv[ii]/100. #divide all WV percentages by 100
 
-    indices = np.where(wv==0.5)[0][:]
-    if len(indices)!=0: #change 50% water vapour values to 20%
-        wv[indices] = 0.2
+    indeces = np.where(wv==0.5)[0][:]
+    if len(indeces)!=0: #change 50% water vapour values to 20%
+        wv[indeces] = 0.2
 
     return iq,cc,bg,wv 
 
-#read single values of condition contraints 
-def convert(iq,cc,bg,wv):
-    newiq = 0
-    newcc = 0
-    newbg = 0
-    newwv = 0
 
-    #print(iq,cc,bg,wv)
-    if (iq[0:3]!='Any') and (iq[0:4]!='null'):
-        newiq = float(iq[0:2])/100
-        if newiq==0.7: #change image qualities of 70% to 50%
-            newiq = 0.5
-    else:
-        newiq = 1.
-
-    if (cc[0:3]!='Any') and (cc[0:4]!='null'):
-        newcc = float(cc[0:2])/100
-    else:
-        newcc = 1.
-
-    if (bg[0:3]!='Any') and (bg[0:4]!='null'):
-        newbg = float(bg[0:2])/100
-    else:
-        newbg = 1. 
-
-    if (wv[0:3]!='Any') and (wv[0:4]!='null'):
-        newwv = float(wv[0:2])/100
-        if newwv==0.5: #change water vapours of 50% to 20%
-            newwv = 0.2
-    else:
-        newwv = 1.   
-    #print(newiq,newcc,newbg,newwv)
-
-    return newiq,newcc,newbg,newwv 
--- gemini_programs.py
+++ gemini_programs.py
@@ -4,6 +4,10 @@ from astropy.time import Time
 
 __all__ = ["Gemini_programs"]
 
+
+
+
+
 def conv_time(array_times):
     """
     Convert time string in form hh:mm:ss.ss to hours
@@ -13,7 +17,12 @@ def conv_time(array_times):
     hour = [(float(value[0]) + float(value[1])/60 + float(value[2])/3600)*u.h for value in values]
     return hour
 
-class Gemini_programs(object):
+
+
+
+
+
+class Gprogram(object):
     
     """ 
     A container class for information about a program's 
@@ -57,5 +66,79 @@ class Gemini_programs(object):
         self.too_status = too_status
         self.scirank = scirank
         self.obs = obs 
+
+
+
+class Gobservation(object):
+    
+    """ 
+    A container class for information about a program's 
+    status, partners, observations... 
+
+    Definitions
+    --------
+    gemprgid        (string)                unique program id
+    partner         (string/int index)      partner name
+    pi              (string)                principle investigator
+    allocated_time  (float)                 hour quantity of allocated program time
+    program_time    (float)                 hour quantity of time charged to program
+    parter_time     (float)                 hour quantity of time charged to partner
+    active          (boolean)               program ready for scheduling 
+    progstart       (astropy.time)          date of program activation
+    progend         (astropy.time)          date of program deactivation
+    completed       (boolean)  
+    too_status      (integer)               1=None, 2=standard, 3=rapid
+    scirank         (integer)               Science rank, band, or TAC ranking
+    obs             (list of objects)?      Identifiers of related observations
+    
+    """
+    
+    #@u.quantity_input(allocated_time=u.h, charged_time=u.h, partner_time=u.h)
+    def __init__(self, ra=None, dec=None, ZD=None, \
+                HA=None, AZ=None, AM=None, \
+                mdist=False, sbcond=None, weight=None, iobswin=False, \
+                wmax=1):
+
+        self.ra = ra
+        self.dec = dec
+        self.ZD = ZD
+        self.HA = HA
+        self.AZ = AZ
+        self.AM = AM
+        self.mdist = mdist
+        self.sbcond = sbcond
+        self.weight = weight
+        self.iobswin = iobswin
+        self.wmax = wmax
+
+
+class Ggroup(object):
+    
+    """ 
+    A container class for information about a program's 
+    status, partners, observations... 
+    
+
+    Definitions
+    --------
+    gemgrid         (string)                unique group id
+    name            (string)                string name
+    type            (string/int.val)        folder/scheduling/other
+    total_time      (float)                 hour quantity of total time
+    timing_constraints                      inherent from observations?
+    
+    """
+    
+    #@u.quantity_input(allocated_time=u.h, charged_time=u.h, partner_time=u.h)
+    def __init__(self, gemgrid=None, name=None, nametype=None, \
+                total_time=None, observations=None, timing_constraints=None):
+
+        self.gemgrid = gemgrid
+        self.name = name
+        self.type = nametype
+        self.total_time = conv_time(total_time)
+        self.observations = observations
+        self.timing_constraints = timing_constraints
+
     
 
