5ce8ccbc04a3f428a33b1589b990d12788c1cb81
Matt Bonnyman <mbonnyman@gemini.edu>
2018-05-31 16:02:23 -0400

    Improved plot format. Will eventually redo altogether

--- amplot.py
+++ amplot.py
@@ -7,27 +7,20 @@ def amplot(obslist,plan,prog_status):
     thn=1
 
     uniq_id,uniq_rr = np.unique(plan['isel'], return_inverse=True)
-    print('uniq_id',uniq_id)
-    print('uniq_rr',uniq_rr)
 
-    plt.plot_date(plan['UT'].plot_date,plan['mAM'],linestyle=':',label='Moon',linewidth=thn,color='grey')
+    plt.plot_date(plan['UT'].plot_date,plan['mAM'],linestyle=':',label='Moon',linewidth=thn,color='grey',markersize=0)
 
     for i in range(len(uniq_id)):
         iimax = int(uniq_id[i])
         if iimax>0:
             ii = np.where(plan['isel']==iimax)[0][:]
             np.append(ii,np.amax(ii)+1)
-            # print('ii',ii)
             temp = plan['UT'][ii]
-            # print('temp',temp)
-            # print('plan[\'UT\'][ii].plot_date',plan['UT'][ii].plot_date)
-            # print('obslist[iimax][\'AM\'][ii]',obslist[iimax]['AM'][ii])
-            plt.plot_date(plan['UT'].plot_date,obslist[iimax]['AM'],linestyle='-',linewidth=thn,color='black')
-            plt.plot_date(plan['UT'][ii].plot_date,obslist[iimax]['AM'][ii],linestyle='-',linewidth=thk)
-            
-            print('Obs ID: ',prog_status['obs_id'][iimax])
-            print('Observation information: ',obslist[iimax])
+            # print('Plotting: ',prog_status['obs_id'][iimax])
+            plt.plot_date(plan['UT'].plot_date,obslist[iimax]['AM'],linestyle='-',linewidth=thn,color='black',markersize=0)
+            plt.plot_date(plan['UT'][ii].plot_date,obslist[iimax]['AM'][ii],linestyle='-',linewidth=thk,markersize=0)
 
-    plt.ylim(2, 1)
+    plt.ylim(2.1, 0.9)
     plt.show()
-    exit()
+    
+    return
