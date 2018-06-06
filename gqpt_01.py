fa848e4f33b6dcf14ba8add746c4b8efb78cb22a
Matt Bonnyman <mbonnyman@gemini.edu>
2018-06-06 15:37:20 -0400

    Made changes to program organization

--- gqpt_01.py
+++ gqpt_01.py
@@ -1,3 +1,6 @@
+import time as t
+timer = t.time()
+
 import argparse
 import re
 import numpy as np
@@ -5,21 +8,25 @@ import textwrap
 import astropy.units as u
 from astroplan import (download_IERS_A,Observer)
 from astropy import (coordinates,time)
-from read_cat import Catalog
-import convcond
-import elevconst
-import queueplanner as queueplanner  
-from gemini_programs import Gprogram
-import convert_input
+import convconst
+from calc_weights import calc_weights
+from gemini_classes import Gcatalog2018,Gprogstatus,Gcatfile,Gcondition,Gelevconst
 from amplot import amplot
 from printplan import printplan
+from gemini_schedulers import priority_scheduler
 
-#starttime = time.time()
-#print('Time to read: ',time.time()-starttime)
+print('Time to import: ',t.time()-timer)
+timer = t.time() #reset timer
+
+
+
 
 #   =============================================== Read and command line inputs =============================================================
 
-fprint = '\t{0:<30s}{1}' #print format
+verbose = False
+
+aprint = '\t{0:<30s}{1}' #print two strings
+bprint = '\t{0:<30s}{1:<.4f}' #print string and number
 
 parser = argparse.ArgumentParser(prog='gqpt.py',
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -46,8 +53,6 @@ parser = argparse.ArgumentParser(prog='gqpt.py',
                         -w   --wv               Water vapor constraint   [DEFAULT=Any]
 
                         -u   --update           Download up-to-date IERS(International Earth Rotation and Reference Systems).
-
-                        -v   --verbose          Toggle verbose mode [DEFAULT=False] 
 
                     *****************************************************************************************************                        
                                             '''))
@@ -85,21 +90,21 @@ parser.add_argument('-u', '--update',\
                     action='store_true',\
                     default=False)
 
-parser.add_argument('-v', '--verbose',\
-                    action='store_true',\
-                    default=False)
-
 parse = parser.parse_args()
 otfile = parse.otfile
 site_name = parse.observatory
-dst = parse.dst
-verbose = parse.verbose
-update = parse.update
 startdate = parse.startdate
 enddate = parse.enddate
+dst = parse.dst
+update = parse.update
 
 if update: #download up to date International Earth Rotation and Reference Systems data
     download_IERS_A()
+
+
+
+
+#   ========================================= Input conditions =====================================================
 
 #Format condition constraint input
 if parse.iq == 'Any':
@@ -116,125 +121,172 @@ else:
     wv = parse.wv[0:2] + '%'
 
 
-#   ================================================ Select observatory ==============================================================
+
+
+#   ======================================== Select observatory =====================================================
 #   Set site name for Cerro Pachon or Mauna Kea and initialize Observer object
 
 # Check observatory input. return timezone, utc-local time difference
-site_name,timezone_name,utc_to_local = convert_input.site(site_name=site_name,dst=dst)
+if np.logical_or(site_name=='gemini_south',site_name=='CP'):
+    site_name = 'gemini_south'
+    timezone_name = 'Chile/Continental'
+    if dst == True:
+        utc_to_local = -3.*u.h
+    else:
+        utc_to_local = -4.*u.h 
+elif np.logical_or(site_name=='gemini_north',site_name=='MK'):
+    site_name = 'gemini_north'
+    timezone_name = 'US/Hawaii'
+    utc_to_local = -10.*u.h
+else:
+    print('Input error: Could not determine observer location and timezone. Allowed inputs are \'gemini_south\', \'CP\'(Cerro Pachon),\'gemini_north\', or \'MK\'(Mauna Kea).)')
+    exit()
 
 site = Observer.at_site(site_name) #initialize Observer object
 #can add timezone=timezone_name later if desired (useful if pytz objects are ever used)
 
 print('')
-print(fprint.format('Site: ',site.name))
-print(fprint.format('Height: ',site.location.height))
-print(fprint.format('Longitude: ',coordinates.Angle(site.location.lon)))
-print(fprint.format('Latitude: ',coordinates.Angle(site.location.lat)))
+print(aprint.format('Site: ',site.name))
+print(bprint.format('Height: ',site.location.height))
+print(bprint.format('Longitude: ',coordinates.Angle(site.location.lon)))
+print(bprint.format('Latitude: ',coordinates.Angle(site.location.lat)))
+
 
 
 
 #   ===================== Read input start/end dates. Create time object for current local and utc time =======================
-local_start, day_nums = convert_input.dates(startdate=startdate,enddate=enddate,utc_to_local=utc_to_local)
+
+dform = re.compile('\d{4}-\d{2}-\d{2}') #yyyy-mm-dd format
+
+if startdate == None:
+    current_time = time.Time.now() + utc_to_local    
+    local_start = time.Time(str(current_time)[0:10] + ' 18:00:00.00') #18:00 local on first night
+else:
+    if dform.match(startdate): #check startdate format
+        local_start = time.Time(startdate + ' 18:00:00.00') #18:00 local on first night
+    else:
+        print('\nInput error: \"'+startdate+'\" not a valid start date.  Must be in the form \'yyyy-mm-dd\'')
+        exit()
+
+if enddate == None: #default number of observation nights is 1
+    day_nums = [0]*u.d
+else:
+    if dform.match(enddate): #check enddate format
+        end_time = time.Time(enddate+' 18:00:00.00') #time object of 18:00:00 local time  
+        d = int((end_time - local_start).value + 1) #number of days between startdate and enddate
+        if d >= 0: 
+            day_nums = np.arange(d)*u.d #list of ints with day units e.g. [0*u.d,1*u.d,2*u.d,...]
+        else: 
+            print('\nInput error: Selected end date \"'+enddate+'\" is prior to the start date.')
+            exit()
+    else:
+        print('\nInput error: \"'+enddate+'\" not a valid end date.  Must be in the form \'yyyy-mm-dd\'')
+        exit()
+
 utc_start = local_start - utc_to_local
 
 print('')
-print(fprint.format('Schedule start (local): ',local_start.iso))
-print(fprint.format('Schedule start (UTC): ',utc_start.iso))
-print(fprint.format('Julian date (UTC): ',utc_start.jd))
-print(fprint.format('Julian year (UTC): ',utc_start.jyear))
-print(fprint.format('Number of nights: ',len(day_nums)))
+print(aprint.format('Schedule start (local): ',local_start.iso))
+print(aprint.format('Schedule start (UTC): ',utc_start.iso))
+print(bprint.format('Julian date (UTC): ',utc_start.jd))
+print(bprint.format('Julian year (UTC): ',utc_start.jyear))
+print(aprint.format('Number of nights: ',len(day_nums)))
 
 
 
-#   ================================== Read catalog and select 'ready' observations for queue ================================================
 
-#read ot catalog and create object containing observation information. 
-otcat = Catalog(otfile)
+#   ================================== Read catalog and select observations for queue ================================================
+
+#Simple object containing catalog information and column headers. 
+catinfo = Gcatfile(otfile)
  
-#Temporary work around as a conditional for (ra!=0 and dec!=0) conditions. 
-ratemp = np.float64(otcat.ra)
-dectemp = np.float64(otcat.dec)
+#Interpret and sort catalog data into appropriate columns(as done in Bryan Miller's IDL version)
+otcat = Gcatalog2018(catinfo=catinfo) 
 
-#Select observations for queue 
+#Get indices of observations for queue
 i_obs = np.where(np.logical_and(np.logical_or(otcat.obs_status=='Ready',otcat.obs_status=='Ongoing'),\
     np.logical_or(otcat.obs_class=='Science',\
     np.logical_and(np.logical_or(otcat.inst=='GMOS',otcat.inst=='bHROS'),\
     np.logical_or(otcat.obs_class=='Nighttime Partner Calibration',otcat.obs_class=='Nighttime Program Calibration')))))[0][:] #get indeces of observation to queue
 
 n_obs = len(i_obs)
-print('\n'+str(n_obs)+' observations added to queue...')
+print('')
+print('\t'+str(n_obs)+' observations selected for queue...')
 
 
 
 
 #   ======================== Convert program times to hours, covnert elevation constraints, convert observation conditions.  =====================================
 #   Create dictionary structures and empty arrays
-cond = np.empty(n_obs,dtype={'names':('iq','cc','bg','wv'),'formats':('f8','f8','f8','f8')})
-prog_status = np.empty(n_obs,dtype={'names':('prog_id','obs_id','target','band','comp_time','tot_time','obs_time'),'formats':('U30','U30','U60','i8','f8','f8','f8')})
+
 elev_const = np.empty(n_obs,dtype={'names':('type','min','max'),'formats':('U20','f8','f8')})
-comp_time = np.zeros(n_obs)
-tot_time = np.zeros(n_obs)
-obs_time = np.zeros(n_obs)
 
-def hms_to_hr(timestring):
+def hms_to_hr(timestring): #convert 'HH:MM:SS' string to hours
     (h, m, s) = timestring.split(':')
-    return (int(h) + int(m)/60 + int(s)/3600)
+    return (np.int(h) + np.int(m)/60 + np.int(s)/3600)
 
+charged_time = np.zeros(n_obs)
+planned_exec_time = np.zeros(n_obs)
 for i in range(0,n_obs): #cycle through selected observations
-    
     #compute observed/total time, add additional time if necessary
-    temp_comp_time = hms_to_hr(otcat.charged_time[i_obs[i]])
-    temp_tot_time = hms_to_hr(otcat.planned_exec_time[i_obs[i]])
-    
-    if (temp_comp_time>0):
+    temp_ct = hms_to_hr(otcat.charged_time[i_obs[i]])
+    temp_pet = hms_to_hr(otcat.planned_exec_time[i_obs[i]])
+    if (temp_ct>0.):
         if (otcat.disperser[i_obs[i]]=='Mirror'):
-            temp_tot_time = temp_tot_time+0.2
+            temp_pet = temp_pet + 0.2
         else:
-            temp_tot_time = temp_tot_time+0.3
-    comp_time[i] = temp_comp_time
-    tot_time[i] = temp_tot_time
+            temp_pet = temp_pet + 0.3
+    charged_time[i] = temp_ct
+    planned_exec_time[i] = temp_pet
 
-    #compute and fill elev_const structure
-    elev_const[i]=elevconst.convert(otcat.elev_const[i_obs[i]])
+# create elev_const object
+elev_const = Gelevconst(otcat.elev_const[i_obs])
 
-#convert cond and fill cond dictionary
-cond['iq'],cond['cc'],cond['bg'],cond['wv']=convcond.convert_array(otcat.iq[i_obs],otcat.cloud[i_obs],otcat.sky_bg[i_obs],otcat.wv[i_obs])
+# create condition object
+cond = Gcondition(iq=otcat.iq[i_obs],cc=otcat.cc[i_obs],bg=otcat.bg[i_obs],wv=otcat.wv[i_obs])
 
-#fill prog_status dictionary with program status information of observations selected for queueing
-prog_status['prog_id']=otcat.prog_ref[i_obs]
-prog_status['obs_id']=otcat.obs_id[i_obs]
-prog_status['target']=otcat.target[i_obs]
-prog_status['band']=otcat.band[i_obs]
-prog_status['comp_time']=comp_time/tot_time
-prog_status['tot_time']=tot_time
-prog_status['obs_time']=obs_time
+# create program status object
+prog_status = Gprogstatus(prog_id=otcat.prog_ref[i_obs], obs_id=otcat.obs_id[i_obs], target=otcat.target[i_obs],\
+                        band=otcat.band[i_obs], comp_time=charged_time/planned_exec_time, \
+                        tot_time=planned_exec_time*u.h, obs_time=np.zeros(n_obs)*u.h)
 
-# print(elev_const)
-# print(candidates)
+if verbose:
+    print('charged_time/planned_exec_time',prog_status.comp_time)
+    print('charged_time',charged_time*u.h)
+    print('planned_exec_time',planned_exec_time*u.h)
+    print('elev_const',elev_const)
+    print('cond',cond)
 
+
+    
 
 #   ========================================================= Begin Queueing ======================================================================
 
-for i_day in day_nums:
+print('\nTime to read and prepare catalog data: ',t.time()-timer)
+
+for i_day in day_nums: #cycle through observation days
 
     night_start = local_start + i_day
     night_start_utc = night_start - utc_to_local
-
-    print('\n____________________ Night of '+str(night_start)[0:10]+' ________________________')
     
+    print('\n\t___________________________ Night of '+str(night_start)[0:10]+' _______________________________')
+
     actual_cond = [iq,cc,'Any',wv]
 
-    #make plan for single night
-    obslist,plan = queueplanner.plan_day(i_day=i_day, i_obs=i_obs,\
+    obslist, plan, prog_status = calc_weights(i_day=i_day, i_obs=i_obs,\
                                         n_obs=n_obs, otcat=otcat, site=site, prog_status=prog_status,\
                                         cond=cond, actual_cond=actual_cond, elev_const=elev_const,\
-                                        utc_time=night_start_utc, local_time=night_start, verbose=verbose)
+                                        utc_time=night_start_utc,local_time=night_start)
+    
+    prior_plan, prior_obslist, prior_prog_status = priority_scheduler(i_obs=i_obs, n_obs=n_obs, \
+                                                                    obslist=obslist, plan=plan,\
+                                                                    prog_status=prog_status, otcat=otcat)
 
-    #print plan details
-    printplan(i_obs,obslist,plan,prog_status,otcat,utc_to_local)
+    printplan(i_obs=i_obs, plan=prior_plan, obslist=prior_obslist,\
+                prog_status=prior_prog_status,otcat=otcat,time_diff_utc=utc_to_local)
 
-    #plot airmass
-    #amplot(obslist,plan,prog_status)
+    
+    amplot(obslist=prior_obslist,plan=prior_plan,prog_status=prior_prog_status)
 
     
 
