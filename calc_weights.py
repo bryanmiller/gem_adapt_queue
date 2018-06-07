Fixes: fixed a cmatch weighting error.  Troubleshot other portions of the weighting and scheduling portions of code.  Still attempting to refine algorithm to match IDL output.

calc_weights.py
@@ -106,7 +106,6 @@ def calc_weights(i_day,i_obs,n_obs,otcat,site,prog_status,cond,actual_cond,\
 
 
 
-
 #   =================================== Get time dependent quanitites for Sun & Moon ==================================================   
 
     if verbose: print('\nComputing moon data...')
@@ -367,9 +366,6 @@ def calc_weights(i_day,i_obs,n_obs,otcat,site,prog_status,cond,actual_cond,\
         else:
             obslist[i]['weight'] = np.zeros(n_timesteps)
 
-
-
-
         #   ===================== Check observation windows =======================
 
         nttime = np.int(np.round(ttime/dt)) #get number of time steps needed for observation
@@ -386,6 +382,7 @@ def calc_weights(i_day,i_obs,n_obs,otcat,site,prog_status,cond,actual_cond,\
             
             obslist[i]['iobswin'] = [ii[0],ii[-1]]
             obslist[i]['wmax'] = obslist[i]['weight'][i_wmax]
+
         else:
             obslist[i]['iobswin'] = [-1,-1]
             obslist[i]['wmax'] = -1

obsweight.py
@@ -67,22 +67,28 @@ def obsweight(cond, dec, AM, HA, AZ, band, user_prior, status, latitude,\
      #decrease weight if conditions are better than requested iq,cc.
      # *effectively drop one band if IQ or CC are better than needed
      # and not likely to lose target, need to make this wavelen. dep. 
-     better_iq = acond['iq']<cond['iq']
-     better_cc = acond['cc']<cond['cc']
-
      if verbose: 
+          better_iq = acond['iq']<cond['iq']
+          better_cc = acond['cc']<cond['cc']
           print('iq better than required',better_iq)
           print('cc better than required',better_cc)
 
-     i_better_cond = np.where(np.logical_or(better_iq,better_cc))[0][:]
-     if len(i_better_cond)!=0:
-          cmatch[i_better_cond] = cmatch[i_better_cond] * 0.75
+     i_better_iq = np.where(acond['iq']<cond['iq'])[0][:]
+     if len(i_better_iq)!=0:
+          cmatch[i_better_iq] = cmatch[i_better_iq] * 0.75
+
+     i_better_cc = np.where(acond['cc']<cond['cc'])[0][:]
+     if len(i_better_cc)!=0:
+          cmatch[i_better_cc] = cmatch[i_better_cc] * 0.75
 
      if verbose: print('cmatch',cmatch)
      
      # Compute single weight representative of conditions 
      twcond = (1./cond['iq'])**3 + (1./cond['cc'])**3 + (1./cond['bg'])**3 + (1./cond['wv'])**3
-     if verbose: print('twcond',twcond)
+     
+     if verbose: 
+          print('wiq,wcc,wbg,wwv',(1./cond['iq'])**3, (1./cond['cc'])**3, (1./cond['bg'])**3, (1./cond['wv'])**3)
+          print('twcond',twcond)
 
      # ======================== Airmass ========================
      i_bad_AM = np.where(AM>2.)[0][:]