9896d90ed6f848e4ea2a54762ab14b59632bb78e
Bryan Miller <millerwbryan@gmail.com>
2018-09-03 15:55:58 -0300

    Remove old files

--- intervals.py
+++ intervals.py
@@ -1,44 +1,52 @@
+# Matt Bonnyman 18 July 2018
+
 import numpy as np
+
 
 def intervals(indices):
     """
-    Find the number and properties of contiguous intervals for an array of indices
+    Find the number and properties of contiguous intervals for an array of indices.
 
     Parameters
     ----------
-    indices : np.array of integers
-        Array of indices
+    indices : np.ndarray of ints
+        array of indices
 
     Returns
     -------
-    integer array
+    indx : np.ndarray of ints
 
     """
 
     ni = len(indices)
-    cvec = np.zeros(ni,dtype=int)
-    nint=1
+    cvec = np.zeros(ni, dtype=int)
+    nint = 1
     cvec[0] = nint
-    for j in range(1,ni):
+    for j in range(1, ni):
         if (indices[j] != (indices[j - 1] + 1)):
-            nint=nint+1
+            nint = nint + 1
         cvec[j] = nint
 
-    indx = np.digitize(cvec,bins=np.arange(ni)+1)
-    
+    indx = np.digitize(cvec, bins=np.arange(ni) + 1)
+
     return indx
 
-if __name__=='__main__':
+
+def test_intervals():
     print('\nTest intervals.py...')
     print('Get intervals of continuous -1s...')
 
     indices = np.array([0, 0, 0, 0, -1, -1, -1, -1, 2, 2, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, -1, -1, -1, -1], dtype=int)
-    ints = np.zeros(len(indices),dtype=int)
+    ints = np.zeros(len(indices), dtype=int)
     i_negones = np.where(indices == -1)[0][:]
     ints[i_negones] = intervals(i_negones)
 
     print('input:', indices)
     print('output: ', ints)
 
-    assert((ints == np.array([0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2])).all())
+    assert ((ints == np.array([0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2])).all())
     print('Test successful!')
+
+
+if __name__ == '__main__':
+    test_intervals()
--- select_obs.py
+++ select_obs.py
@@ -1,14 +1,154 @@
 import numpy as np
 import astropy.units as u
 
-def selectqueue(catinfo):
+import weights as weights
+
+
+def _getcalday(instcal, date):
+    """
+    Get index of row for current night in instrument calendar table.
+    """
+    for i in range(len(instcal['date'])):
+        if instcal['date'][i] in date:
+            return i
+    print('\n Date '+date+' not found in instrument configuration calendar.')
+    raise ValueError('Date '+date+' not found in instrument configuration calendar.')
+
+
+def _checkinst(inst, disp, fpu, insttable):
+    """
+    Check observation instrument requirements against tonight's instrument configuration.
+
+
+    Parameters
+    ----------
+    inst : str
+        Observation instrument
+
+    disp : str
+        Observation instrument disperser (or 'null')
+
+    fpu : str
+        Observation instrument focal plane unit (or 'null')
+
+    insttable : '~astropy.table.Table'
+        Instrument configuration calender.
+
+    i_cal : int
+        Index of row for tonights instrument configuration.
+
+    Returns
+    -------
+    boolean. True or False if the observation requirements are satisfied by the current configuration.
+    """
+
+    # print('\ninst, disp, fpu: ',inst,disp,fpu)
+    if inst not in insttable['insts']:
+        # print('Not available')
+        return False
+    elif inst in insttable['insts']:
+        # print('Available')
+        if 'GMOS' in inst:
+            # print('GMOS')
+            return ((disp in insttable['gmos_disp']) or ('null' in insttable['gmos_disp'])) and \
+                   ((fpu in insttable['gmos_fpu']) or ('null' in insttable['gmos_fpu']))
+        elif 'Flamingos' in inst:
+            # print('Flamingos')
+            return (fpu == insttable['f2_fpu']) or (insttable['f2_fpu'] == 'null')
+        else:
+            # print('Not GMOS or F2')
+            return True
+
+
+def i_progs(gemprgid, prog_ref):
+    """
+    Match program identifier strings in prog_ref to strings in gemprgid.
+    Return array of indices.
+
+    Parameters
+    ----------
+    gemprgid : list of strings
+        Gemini program id column from program table
+
+    prof_ref :
+        Gemini program id column from observation table
+
+    Returns
+    -------
+    i_progs : int array
+        Array of indices.
+
+    """
+
+    verbose = False
+
+    i_progs = np.full(len(prog_ref), -1)
+    prog_refs = np.unique(prog_ref)
+
+    if verbose:
+        print(prog_refs)
+
+    for prog in prog_refs:
+        j = np.where(gemprgid == prog)[0][0]  # should only have one value in array
+        jj = np.where(prog_ref == prog)[0][:]  # observations in queue from program 'prog'
+        i_progs[jj] = j
+        if verbose:
+            print(prog, j, jj)
+
+    return i_progs
+
+
+def instconfig(inst, disp, fpu, insttable, date):
+
+    """
+    Select observations to add to tonight's queue.
+
+    Parameters
+    ----------
+    obs : '~astropy.table.Table'
+        All observations in queue
+
+    insttable : '~astropy.table.Table'
+        Instrument configuration calender.
+
+    date : str
+        tonights date
+
+    Returns
+    -------
+    numpy integer array
+        indices of observations selected for tonight's queue
+    """
+    verbose = False
+
+    i_cal = _getcalday(instcal=insttable, date=date)
+    bools = [_checkinst(inst=inst[j], disp=disp[j], fpu=fpu[j], insttable=insttable[i_cal]) for j in range(len(inst))]
+
+    if verbose:
+        print(' \n Verbose: select_obs.selectinst...')
+        print(' Date: ', insttable['date'][i_cal])
+        print(' Available inst.: ', insttable['insts'][i_cal])
+        print(' GMOS disperser: ', insttable['gmos_disp'][i_cal])
+        print(' GMOS fpu: ', insttable['gmos_fpu'][i_cal])
+        print(' F2 fpu: ', insttable['f2_fpu'][i_cal])
+        print('')
+        vprint = '\t{0:12.10} {1:12.10} {2:12.10} {3:12.10}'
+        print(vprint.format('Boolean', 'Inst.', 'Disperser', 'FPU'))
+        print(vprint.format('-------', '-----', '---------', '---'))
+        for k in range(len(inst)):
+            print(vprint.format(str(bools[k]), inst[k], disp[k], fpu[k]))
+
+    return np.where(bools)[0][:]
+
+
+def selectqueue(cattable):
 
     """
     Select observations from catalog to add to Queue.
 
     Parameters
     ----------
-    catinfo : '~gemini_otcat.Gcatfile'
+    cattable : '~gemini_otcat.Gcatfile'
         OT catalog info
 
     Returns
@@ -16,184 +156,8 @@ def selectqueue(catinfo):
     numpy integer array
         indices of observations selected for queue
     """
-    return np.where(np.logical_and(np.logical_or(catinfo.obs_status == 'Ready', catinfo.obs_status == 'Ongoing'),
-                                   np.logical_or(catinfo.obs_class == 'Science',
-                                   np.logical_and(np.logical_or(catinfo.inst == 'GMOS', catinfo.inst == 'bHROS'),
-                                   np.logical_or(catinfo.obs_class == 'Nighttime Partner Calibration',
-                                                 catinfo.obs_class == 'Nighttime Program Calibration')))))[0][:]
-
-def selectinst(i_obs, obs, instcal, datestring):
-
-    """
-    Select observations to add to tonight's queue.
-
-    Parameters
-    ----------
-    obs : '~gemini_otcat.Gobservations'
-        All observations in queue
-
-    inst_calender :
-        Instrument and component calender
-
-    Returns
-    -------
-    numpy integer array
-        indices of observations selected for tonight's queue
-    """
-
-    def getcalday(instcal, datestring):
-        for i in range(len(instcal['date'])):
-            if instcal['date'][i] in datestring:
-                return i
-        print('\n Error: Date '+datestring+' not found in instrument configuration calendar.')
-        raise ValueError
-
-    def checkinst(inst, disp, fpu, instcal):
-        """
-        Check observation instrument requirements against tonight's instrument configuration.
-
-
-        Parameters
-        ----------
-        inst : str
-            Observation instrument
-
-        disp : str
-            Observation instrument disperser (or 'null')
-
-        fpu : str
-            Observation instrument focal plane unit (or 'null')
-
-        instcal : '~gemini_instruments.Instruments'
-            Instrument configuration calender object.
-
-        i_cal : int
-            Index for current date intrument configuration in instcal
-
-        Returns
-        -------
-        boolean. True or False if the observation requirements are satisfied by the current configuration.
-        """
-
-        # print('\ninst, disp, fpu: ',inst,disp,fpu)
-        if inst not in instcal['insts']:
-            # print('Not available')
-            return False
-        elif inst in instcal['insts']:
-            # print('Available')
-            if 'GMOS' in inst:
-                # print('GMOS')
-                return ((disp in instcal['gmos_disp']) or ('null' in instcal['gmos_disp'])) and \
-                       ((fpu in instcal['gmos_fpu']) or ('null' in instcal['gmos_fpu']))
-            elif 'Flamingos' in inst:
-                # print('Flamingos')
-                return (fpu == instcal['f2_fpu']) or (instcal['f2_fpu'] == 'null')
-            else:
-                # print('Not GMOS or F2')
-                return True
-
-    i_cal = getcalday(instcal=instcal, datestring=datestring)
-    bools = [checkinst(inst=obs.inst[j], disp=obs.disperser[j], fpu=obs.fpu[j], instcal=instcal[i_cal]) for j in
-             i_obs]
-
-    verbose = False
-    if verbose:
-        print(' \n Verbose: select_obs.selectinst...')
-        print(' Date: ', instcal['date'][i_cal])
-        print(' Available inst.: ', instcal['insts'][i_cal])
-        print(' GMOS disperser: ', instcal['gmos_disp'][i_cal])
-        print(' GMOS fpu: ', instcal['gmos_fpu'][i_cal])
-        print(' F2 fpu: ', instcal['f2_fpu'][i_cal])
-        print('')
-        vprint = '\t{0:12.10} {1:12.10} {2:12.10} {3:12.10}'
-        print(vprint.format('Boolean', 'Inst.', 'Disperser', 'FPU'))
-        print(vprint.format('-------', '-----', '---------', '---'))
-        for k in i_obs:
-            print(vprint.format(str(bools[k]), obs.inst[k], obs.disperser[k], obs.fpu[k]))
-
-    return np.where(bools)[0][:]
-
-def selecttimewindow(time_consts, timeinfo):
-
-    """
-    Check if there is overlap between an observations time constraints
-    and the current observing window in TimeInfo.  Return True or False.
-
-    Parameters
-    ----------
-    time_consts : list of dictionaries
-        Gemini observation time constraints (eg. time_consts = [{'start': '~astropy.time.Time',
-            'duration': '~astropy.units.Quantity', 'repeat': int, 'period': '~astropy.time.Time'},...])
-
-    timeinfo : '~gemini_classes.TimeInfo'
-        observing period info for the current night
-
-    Returns
-    -------
-    boolean
-    """
-    verbose = False
-    if verbose:
-        print('\n Window: ',timeinfo.start.iso,timeinfo.end.iso)
-        print(' Time const: ')
-
-    if time_consts is None:
-        if verbose: print('\t'+str(time_consts))
-        return True
-    else:
-        for time_const in time_consts: # Cycle through constraints
-
-            # Check case of infinite window length
-            if time_const['duration']==-1.0:
-                if verbose: print('\t' + str(time_const['start'].iso) + ' to forever')
-                if (time_const['start']-timeinfo.end)<0.*u.h:
-                    return True
-                else:
-                    continue
-
-            # Check case of single time window
-            if time_const['repeat']==0:
-                pend = time_const['start']+time_const['duration']
-                if verbose: print('\t' + str(time_const['start'].iso) + ' to ' + str(pend.iso))
-                if (timeinfo.start <= pend) and (time_const['start'] <= timeinfo.end):
-                    return True
-
-            # Check case of repeating time window
-            if time_const['repeat'] != 0:
-                nrepeat = time_const['repeat'] + 1
-                i = 0
-                checknextperiod = True
-                while checknextperiod:
-                    pstart = time_const['start']+time_const['period']*i
-                    pend = pstart + time_const['duration']
-                    if verbose: print('\t' + str(pstart.iso) + ' to ' + str(pend.iso))
-                    if (timeinfo.start <= pend) and (pstart <= timeinfo.end):
-                        return True
-                    else:
-                        i = i+1
-                    if (i>=nrepeat) or (timeinfo.end<pstart):
-                        checknextperiod = False
-
-    # If no cases return True, return False.
-    return False
-
-def selectincomplete(tot_time, obs_time):
-    """
-    Select observation that are incomplete.
-    Computes and rounds remaining observation times to 1/10th hour.
-    Returns true for remaining times > 0
-
-    Parameters
-    ----------
-    tot_time : array of '~astropy.units.Quantity'
-        Total observation times
-
-    obs_time : array of '~astropy.units.Quantity'
-        Amount of tot_time completed/observed.
-
-    Returns
-    -------
-    array of booleans.
-
-    """
-    return np.where(np.round((tot_time - obs_time) * 10) / 10 > 0)[0][:]
+    return np.where(np.logical_and(np.logical_or(cattable['obs_status'] == 'Ready', cattable['obs_status'] == 'Ongoing'),
+                                   np.logical_or(cattable['obs_class'] == 'Science',
+                                   np.logical_and(np.logical_or(cattable['inst'] == 'GMOS', cattable['inst'] == 'bHROS'),
+                                   np.logical_or(cattable['obs_class'] == 'Nighttime Partner Calibration',
+                                                 cattable['obs_class'] == 'Nighttime Program Calibration')))))[0][:]
